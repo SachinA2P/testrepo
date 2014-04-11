5cc934ca9f3208a15680c258309d12266a517d34
Kiran Koduru <crazycreature11@gmail.com>
2014-04-11 14:59:03 -0400

    adding two variables function

--- test1.py
+++ test1.py
@@ -1,2 +1,15 @@
 print "hello World"
 print "Bye world"
+
+def add(x,y):
+	"""
+	Adding two variables
+
+	Returns :
+				Sum of variables
+	"""
+	return x+y
+
+
+print "Sum of variables:"
+print add(3,4)
